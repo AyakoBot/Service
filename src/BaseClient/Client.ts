8d19465c3ae9b9f3347b76fe3f639da07cbf5c97
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2025-11-18 10:16:18 +0100

    chore: remove unused imports and clean up code structure

--- src/BaseClient/Client.ts
+++ src/BaseClient/Client.ts
@@ -1,6 +1,6 @@
 import { API } from '@discordjs/core';
 import { REST } from '@discordjs/rest';
-import { cache as redisRedis } from './Redis.js';
+import { cache as redisCache } from './Redis.js';
 
 export const rest = new REST({ api: 'http://127.0.0.1:8080/api' }).setToken(
  ((process.argv.includes('--dev') ? process.env.DevToken : process.env.Token) ?? '').replace(
@@ -11,4 +11,4 @@ export const rest = new REST({ api: 'http://127.0.0.1:8080/api' }).setToken(
 
 export const api = new API(rest);
 export const user = await api.users.getCurrent();
-export const cache = redisRedis;
+export const cache = redisCache;
