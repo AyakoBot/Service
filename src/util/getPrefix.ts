c5e90d28182f7e2f7e2aea254e179f4b4146bdf4
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2025-12-09 02:06:02 +0100

    feat: add utility functions for string formatting and file resolution

---
+++ src/util/getPrefix.ts
@@ -0,0 +1,20 @@
+import type Client from '../classes/Client.js';
+import Constants from '../classes/Constants.js';
+import GuildSetting from '../plugins/settings/GuildSetting.js';
+
+export default async function (this: Client, msg: RMessage) {
+ if (!msg.guild_id) return Constants.standard.prefix;
+
+ const base = new GuildSetting(this.db, msg.guild_id);
+ const setting = await base.get();
+
+ if (setting?.prefix && msg.content.toLowerCase().startsWith(setting.prefix.toLowerCase())) {
+  return setting.prefix;
+ }
+
+ if (msg.content.toLowerCase().startsWith(Constants.standard.prefix.toLowerCase())) {
+  return Constants.standard.prefix;
+ }
+
+ return undefined;
+}
---
+++ src/util/stp.ts
@@ -0,0 +1,38 @@
+interface R {
+ [key: string]: unknown | R;
+}
+
+/**
+ * Replaces placeholders in a string with values from an object.
+ * @param expression - The string containing placeholders.
+ * @param obj - The object containing values to replace placeholders.
+ * @returns The updated string with placeholders replaced with values.
+ */
+export default (expression: string, obj: R) => {
+ const replace = (substring: string, value: string): string => {
+  const newValue = value.split('.');
+  let decided: R | string = '';
+  const result = obj[newValue[0]];
+  if (!result) return substring;
+  if (newValue.length < 2) return result as string;
+
+  newValue.forEach((element: string, i) => {
+   if (i === 1) decided = (result as R)[element] as string;
+   if (i > 1) decided = (decided as R)[element] as string;
+  });
+
+  return decided as string;
+ };
+
+ expression = expression.replace(/{{\s?([^{}\s]*)\s?}}/g, replace);
+
+ Object.entries(process.env)
+  .filter((e) => e[0].toLowerCase().includes('token'))
+  .forEach((s) => {
+   expression = s[1]
+    ? expression.replace(new RegExp(s[1].replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'g'), '*')
+    : expression;
+  });
+
+ return expression;
+};
