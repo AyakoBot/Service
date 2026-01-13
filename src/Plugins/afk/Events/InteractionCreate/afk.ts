d21416bb34f4b2dcf00d284ddd976af6e64fde48
Larsundso <Larsundso3@gmail.com>
2026-01-13 01:50:12 +0100

    fix: streamline user retrieval in AFK interaction handling

--- src/Plugins/afk/Events/InteractionCreate/afk.ts
+++ src/Plugins/afk/Events/InteractionCreate/afk.ts
@@ -17,16 +17,18 @@ export default async function (this: AFKPlugin, cmd: APIInteraction) {
  if (cmd.type !== InteractionType.ApplicationCommand) return;
  if (!cmd.guild_id) return;
  if (!cmd.channel?.id) return;
- if (!cmd.user?.id) return;
+
+ const user = cmd.user || cmd.member?.user;
+ if (!user?.id) return;
  if (cmd.data.type !== ApplicationCommandType.ChatInput) return;
 
- const member = cmd.member || (await this.client.cache.members.get(cmd.guild_id, cmd.user.id));
+ const member = cmd.member || (await this.client.cache.members.get(cmd.guild_id, user.id));
 
  const reason = cmd.data.options
   ?.filter((o) => o.type === ApplicationCommandOptionType.String)
   .find((o) => o.name === 'reason')?.value as string | undefined;
 
- const afkBase = new AFKState(this.client.db, cmd.user.id, cmd.guild_id);
+ const afkBase = new AFKState(this.client.db, user.id, cmd.guild_id);
  const afk = await afkBase.get();
 
  const embed = new EmbedBuilder()
@@ -42,14 +44,14 @@ export default async function (this: AFKPlugin, cmd: APIInteraction) {
   );
 
  new MessagePayload(this.client)
-  .setContent(await getContent.call(this, cmd.guild_id, afk, cmd.user.id))
+  .setContent(await getContent.call(this, cmd.guild_id, afk, user.id))
   .setChannels([{ id: cmd.channel.id, guildId: cmd.guild_id }])
   .addEmbeds(embed)
   .send();
 
  await afkBase.upsert(
   {
-   userId: cmd.user.id,
+   userId: user.id,
    guildId: cmd.guild_id,
    reason,
    since: Date.now(),
