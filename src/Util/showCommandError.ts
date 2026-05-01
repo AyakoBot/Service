bde12ce26214b4577af822f386fbafbabf000a02
Larsundso <Larsundso3@gmail.com>
2026-05-01 14:54:46 +0200

    fix: refactor showCommandError function to improve error handling and add ephemeral message support

--- src/Util/showCommandError.ts
+++ src/Util/showCommandError.ts
@@ -1,29 +1,23 @@
 import { MessageFlags, type APIInteraction } from 'discord-api-types/v10';
+
 import type Client from '../Classes/Client.js';
 import type { BaseLang } from '../Classes/abstracts/Plugin.js';
-import { MessagePayload } from '../Classes/abstracts/MessagePayload.js';
-import { ContainerBuilder, TextDisplayBuilder } from '@discordjs/builders';
-import { Colors } from '../Types/index.js';
+import getErrorMessagePayload from './getErrorMessagePayload.js';
 
-export default async function (this: Client, error: string, cmd: APIInteraction, t: BaseLang) {
+export default async function (
+ this: Client,
+ error: string,
+ cmd: APIInteraction,
+ t: BaseLang,
+ ephemeral: boolean = true,
+) {
  this.logger.error(`Error executing command: ${error}`);
 
- const api = await this.getAPI(cmd.guild_id);
- if (!api) {
-  this.logger.error('API instance not found for guild:', cmd.guild_id);
-  return;
- }
-
- new MessagePayload(this, {
-  origin: 'showCommandError',
-  reason: 'Showing the executing user an error',
- })
-  .setComponents([
-   new ContainerBuilder()
-    .setAccentColor(Colors.Danger)
-    .addTextDisplayComponents(new TextDisplayBuilder().setContent(`## ${t.t.error()}\n-# ${error}`))
-    .toJSON(),
-  ])
-  .setFlags(MessageFlags.IsComponentsV2)
+ getErrorMessagePayload
+  .call(this, t, error, {
+   origin: 'ShowCommandError',
+   reason: 'Showing the executing user an error',
+  })
+  .setFlags(MessageFlags.IsComponentsV2 | (ephemeral ? MessageFlags.Ephemeral : 0))
   .reply(cmd);
 }
