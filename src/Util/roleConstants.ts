46be076c4c5d583b394d41bdbc922932a07bb526
larsundso <larsundso3@gmail.com>
2026-09-01 13:53:38 +0200

    feat: add role constants for guild role limit and holographic preset colors

---
+++ src/Util/roleConstants.ts
@@ -0,0 +1,8 @@
+export const guildRoleLimit = 250;
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const HolographicPreset = {
+ primaryColor: 11127295,
+ secondaryColor: 16759788,
+ tertiaryColor: 16761760,
+};
