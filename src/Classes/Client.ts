922a13b4ba8a7f45b4c24a7fa71363e0fb393108
Larsundso <Larsundso3@gmail.com>
2026-09-06 17:37:01 +0200

    feat(Client): add 'Welcome' to cutoverFeatures for enhanced functionality

--- src/Classes/Client.ts
+++ src/Classes/Client.ts
@@ -36,7 +36,7 @@ export default class Client {
 
  debugGuilds = ['298954459172700181', '669893888856817665', '672546390915940405', '1518697272525979648', '1499475512911859763', '1468552297473904777'];
  debugUsers = ['318453143476371456', '564052925828038658', '669915074458025984', '1463063234343862437'];
- cutoverFeatures = ['Ticketing', 'Info'];
+ cutoverFeatures = ['Ticketing', 'Info', 'Welcome'];
 
  sendMessageCache: SendMessageCache;
  jobCache: typeof JobCache.prototype;
